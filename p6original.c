d70afe30b8a281912839a587419b9025e9979871
Pinta Ram Ram <97089550+PintaRam@users.noreply.github.com>
2022-03-23 04:56:53 +0000

    Write a program to find the index of a substring of a string.

--- p6original.c
+++ p6original.c
@@ -6,25 +6,53 @@ void input_string(char *a)
 }
 int str_reverse(char *string, char *substring)
 {
-  int i=0,j=0;
+ /* int i=0,j=0;
  // for(i = 0;string[i]==substring [i]; i++) 
   while (substring[i]!='\0')
     if(substring[i]==string[j])
     {
-      i++;
       j++;
+      i++;
+      
     }
   else
   {
     j++;
   }
-  return i;
-  
-  
+  return i;*/
+   int i, j, a;
+   i = 0, j = 0;
+ 
+   while (string[i] != '\0') {
+ 
+      while (string[i] != substring[0] && string[i] != '\0')
+         i++;
+ 
+      if (string[i] == '\0')
+         return (-1);
+ 
+      a = i;
+ 
+      while (string[i] == substring[j] && string[i] != '\0' && substring[j] != '\0') {
+         i++;
+         j++;
+      }
+ 
+      if (substring[j] == '\0')
+         return (a);
+      if (string[i] == '\0')
+         return (-1);
+ 
+      i = a + 1;
+      j = 0;
+   }
 }
+  
+  
+
 void output(char *string, char *substring, int index)
 { 
-  if(index!=0)
+  if(index!=-1)
         printf("The index of %s in %s is %d\n", substring,string,index+1);
   else 
     printf("index does not found");
